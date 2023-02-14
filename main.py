85829359586b8f783be46b3eb8018f64a18f43e7
Marco Burstein <marco@marco.how>
2023-02-14 17:09:43 -0500

    Update with multithreading and actions.

--- main.py
+++ main.py
@@ -1,52 +1,76 @@
 import sys, socket
+import threading
 
 def server(port):
   serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   serversocket.bind(('', port))
   serversocket.listen()
-  s, _ = serversocket.accept()
+
+  server_messages = {}
+  server_client_sockets = {}
+
+  def server_client_handler(s):
+    while True:
+      version = receive_sized_int(s, 1)
+      print('Version:', version)
+      if version == 0:
+        action = receive_sized_int(s, 1)
+        print('Action:', action)
+        if action == 1:
+          # Create
+          size = receive_sized_int(s, 4)
+          body = receive_sized_string(s, size)
+
+          if body in server_messages:
+            print('Already has account.')
+          elif ',' in body:
+            print('Invalid username.')
+          else:
+            server_messages[body] = []
+            server_client_sockets[body] = s
+        elif action == 2:
+          # List
+          pass
+        elif action == 3:
+          # Send
+          size = receive_sized_int(s, 4)
+          body = receive_sized_string(s, size)
+          index = body.index(',')
+          username = body[0:index]
+          text = body[index+1:]
+          server_messages[username].append(text)
+        elif action == 4:
+          # Deliver
+          size = receive_sized_int(s, 4)
+          body = receive_sized_string(s, size)
+          server_client_sockets[body].send(bytes(','.join(server_messages[body]), 'utf-8'))
+        elif action == 5:
+          # Delete
+          size = receive_sized_int(s, 4)
+          body = receive_sized_string(s, size)
+          del server_messages[body]
+          del server_client_sockets[body]
 
   while True:
-    version = receive_sized_int(s, 1)
-    print('Version:', version)
-    if version == 0:
-      action = receive_sized_int(s, 1)
-      print('Action:', action)
-      if action == 1:
-        # Create
-        size = receive_sized_int(s, 4)
-        body = receive_sized_string(s, size)
-        # TODO
-      elif action == 2:
-        # List
-        # TODO
-        pass
-      elif action == 3:
-        # Send
-        size = receive_sized_int(s, 4)
-        body = receive_sized_string(s, size)
-        # TODO
-      elif action == 4:
-        # Deliver
-        size = receive_sized_int(s, 4)
-        body = receive_sized_string(s, size)
-        # TODO
-      elif action == 5:
-        # Delete
-        size = receive_sized_int(s, 4)
-        body = receive_sized_string(s, size)
-        # TODO
+    s, _ = serversocket.accept()
+    threading.Thread(target=server_client_handler, args=(s,)).start()
 
 def client(host, port):
+  def send_action(s, action, body=None):
+    send_sized_int(s, 0, 1)
+    send_sized_int(s, action, 1)
+
+    if body:
+      send_sized_int(s, len(body), 4)
+      s.send(body)
+
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((host, port))
 
-  send_sized_int(s, 0, 1)
-  send_sized_int(s, 1, 1)
-
-  data = b'examplee'
-  send_sized_int(s, len(data), 4)
-  s.send(data)
+  send_action(s, 1, b"test")
+  send_action(s, 2)
+  send_action(s, 3, b"test,this is a test message")
+  send_action(s, 4, b"test")
 
 def receive_sized_int(s, size):
   return int.from_bytes(receive_sized(s, size), 'big')
