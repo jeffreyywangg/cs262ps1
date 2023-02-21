437211f2b6a57a83c486568f77ea4b8d00f9ae97
Marco Burstein <marco@marco.how>
2023-02-21 16:09:44 -0500

    Add GRPC tests.

---
+++ grpc/test.py
@@ -0,0 +1,65 @@
+import threading
+import unittest
+from server import *
+
+class ClientServersTest(unittest.TestCase):
+  def test_signup(self):
+    server = ServerServicer()
+    self.assertTrue(server.Authenticate(service_pb2.AuthenticateRequest(
+      username="test",
+      password="test"
+    ), None).success)
+
+  def test_list(self):
+    server = ServerServicer()
+    auth_a = server.Authenticate(service_pb2.AuthenticateRequest(
+      username="test_a",
+      password="test"
+    ), None)
+    self.assertTrue(auth_a.success)
+    self.assertTrue(server.Authenticate(service_pb2.AuthenticateRequest(
+      username="test_b",
+      password="test"
+    ), None).success)
+
+    response = server.List(service_pb2.ListRequest(
+      token=auth_a.response,
+      request="\n"
+    ), None)
+    self.assertTrue(response.success)
+    self.assertEqual(response.response, 'test_a,test_b')
+
+  def test_send_deliver(self):
+    server = ServerServicer()
+    auth_a = server.Authenticate(service_pb2.AuthenticateRequest(
+      username="test_a",
+      password="test"
+    ), None)
+    auth_b = server.Authenticate(service_pb2.AuthenticateRequest(
+      username="test_b",
+      password="test"
+    ), None)
+
+    self.assertTrue(auth_a.success)
+    self.assertTrue(auth_b.success)
+
+    self.assertTrue(server.Send(service_pb2.SendRequest(
+      token=auth_a.response,
+      username="test_b",
+      body="hello1"
+    ), None).success)
+
+    self.assertTrue(server.Send(service_pb2.SendRequest(
+      token=auth_a.response,
+      username="test_b",
+      body="hello2"
+    ), None).success)
+
+    response = server.Deliver(service_pb2.DeliverRequest(
+      token=auth_b.response,
+    ), None)
+    self.assertTrue(response.success)
+    self.assertEqual(response.response, 'hello1\n\nhello2')
+
+if __name__ == '__main__':
+  unittest.main()
